9e12d28c229ae2e4da11542ce6390b69d960e924
Thomas <thomas@DevSecOpss-MacBook-Pro.local>
2020-11-24 14:52:54 -0500

    all done except for changing answers

--- survey.py
+++ survey.py
@@ -14,17 +14,16 @@
 # Imports
 ############################################################################################
 
-import sys, os, json
+import sys, os, time, json
 from termcolor import colored, cprint
+from progress.bar import FillingCirclesBar
 from datetime import datetime
-import question
 
 ############################################################################################
 # Program Functions
 ############################################################################################
 
 survey_questions = []
-survey_answers = []
 
 survey_description = '''
 Welcome to the developer incentive and job satisfaction survey! This survey will help senior leaders increase
@@ -35,6 +34,8 @@ Do not discuss sensitive operational information, or information injurious to th
 subjects mentioned in AR 360-1, paragraphs 5-3a(1) through 5-3a(20).\n
 '''
 
+timestamp = None
+
 # Question Class
 
 class Question:
@@ -42,18 +43,22 @@ class Question:
         self.category = category
         self.prompt = prompt
         self.choices = choices
-        self.answer = ''
+        self.answer = None
 
 # Print Functions
 
-def printd(s):
-    cprint(s, 'blue', attrs=['bold', 'dark'])
+def printd(s, c='blue'):
+    cprint(s, c, attrs=['bold'])
 
 def printc(s):
-    cprint(s, 'blue', attrs=['bold'])
+    cprint(s, 'magenta', attrs=['bold'])
 
 def printe(s):
     cprint(s, 'red', attrs=['bold', 'reverse', 'blink'])
+
+def printr(s):
+    colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta']
+    for i, l in enumerate(s): cprint(l, colors[i%len(colors)], attrs=['bold'], end='')
 
 # Menu Functions
 
@@ -70,42 +75,98 @@ def main_menu():
 
 def exec_menu(s):
     os.system('clear')
+    print()
     try: 
         actions[s]()
     except KeyError:
-        printe('Invalid selection, try again')
+        printe('Invalid selection, try again.')
         actions['main_menu']()
 
 def take_survey():
-    printd('Menu A!\n')
-    printd('There are '+str(len(survey_questions))+' questions in this survey. Press [Enter] to begin...')
+    printd('Welcome to the beginning of the survey. Please answer to the best of your ability!\n')
+    printd('There are '+str(len(survey_questions))+' questions in this survey. Press [Enter] to begin...\n', 'cyan')
     input()
-    for q in survey_questions:
+    for i,q in enumerate(survey_questions):
         os.system('clear')
-        # CONTINUE HERE
+        printd('Question ' + str(i+1) + ' of ' + str(len(survey_questions)) + '\n')
+        printd(str(i+1) + '. ' + q.prompt)
+        c = None
+        if q.category == 'number':
+            while True:
+                printd('(Please enter a valid number)', 'cyan')
+                try:
+                    c = int(input('\n >> '))
+                    if c < 0: raise ValueError
+                    break
+                except ValueError:
+                    printe('\nInvalid input! Please try again.')
+        elif q.category == 'text':
+            c = input('\n >> ')
+        elif q.category == 'choice':
+            printd('(Choose one of the following by entering the corresponding number)\n', 'cyan')
+            for choice in q.choices:
+                printc('\t'+choice)
+            while True:
+                try:
+                    c = int(input('\n >> '))
+                    if c < 1 or c > len(q.choices): raise ValueError
+                    break
+                except ValueError:
+                    printe('\nInvalid choice! Please try again.')
+        elif q.category == 'list':
+            printd('(This is a list question, press [Enter] to add an entry or to end the list)', 'cyan')
+            l = []
+            while True:
+                c = input('\n >> ')
+                if not c: 
+                    printd('\nDo you want to end the list?\n')
+                    printc('[1] Yes')
+                    printc('[2] No')
+                    choice = input('\n >> ')
+                    if choice == '1':
+                        c = l
+                        break
+                    else:
+                        printd('(Continuing to add to list)', 'cyan')
+                        continue
+                l.append(c)
+                printc('\"' + c + '\" was added to the current list.')
+        q.answer = c
+    os.system('clear')
+    printr('You have completed the survey! Press [Enter] to return to the Main Menu...\n')
+    input()
+    actions['main_menu']()
 
 def view_answers():
-    printd('Menu A!\n')
-    printc('[9] Back to Main Menu')
-    printc('[0] Exit')
-    c = input('\n >> ')
-    exec_menu(c)
+    printd('View Survey!\n')
+    printd('Press [Enter] to return to the Main Menu...\n', 'cyan')
+    printd('\nSurvey Questions\n')
+    for i,q in enumerate(survey_questions):
+        printd('—'*50, 'white')
+        printd('Question ' + str(i+1) + ' of ' + str(len(survey_questions)) + '\n')
+        printd(str(i+1) + '. ' + q.prompt + '\n')
+        if q.category == 'choice':
+            for choice in q.choices:
+                printc('\t'+choice)
+        printd('\nAnswer: ' + str(q.answer), 'white')
+        printd('—'*50+'\n', 'white')
+    printd('Press [Enter] to return to the Main Menu...\n', 'cyan')
+    input()
+    actions['main_menu']()
 
 def change_answers():
-    printd('Menu A!\n')
-    printc('[9] Back to Main Menu')
-    printc('[0] Exit')
-    c = input('\n >> ')
-    exec_menu(c)
+    printd('Function in Progress!\n')
+    pass
+    printd('Press [Enter] to return to the Main Menu...\n', 'cyan')
+    input()
+    actions['main_menu']()
 
 def submit():
-    printd('Menu A!\n')
-    printc('[9] Back to Main Menu')
-    printc('[0] Exit')
-    c = input('\n >> ')
-    exec_menu(c)
-
-def back():
+    printr('Turning file into json format!\n')
+    write_json()
+    loading_bar()
+    printd('Press [Enter] to return to the Main Menu...\n', 'cyan')
+    input()
     actions['main_menu']()
 
 def exit():
@@ -119,13 +180,18 @@ def exit():
     else:
         actions['main_menu']()
 
+def loading_bar():
+    with FillingCirclesBar('Loading', max=20) as bar:
+        for i in range(20):
+            time.sleep(0.1)
+            bar.next()
+
 actions = {
     'main_menu': main_menu,
     '1': take_survey,
     '2': view_answers,
     '3': change_answers,
     '4': submit,
-    '9': back,
     '0': exit,
 }
 
@@ -137,22 +203,35 @@ def get_questions():
         for line in f.readlines():
             c, q, a = map(str.strip, line.split('~'))
             a = a.split(',')
-            for i, choice in enumerate(a):
-                a[i] = '('+str(i+1)+') '+choice.strip()
+            if c == 'choice':
+                for i, choice in enumerate(a):
+                    a[i] = '('+str(i+1)+') '+choice.strip()
+            else:
+                a = None
             if c: survey_questions.append(Question(c, q, a))
 
 def write_json():
-    outfile = 'responses/entry-' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt'
-    with open(file, 'w') as f:
-        json.dump(results, outfile)
-
+    outfile = 'responses/entry-' + timestamp + '.txt'
+    data = []
+    for q in survey_questions:
+        data.append({
+            'type': q.category,
+            'question': q.prompt,
+            'choices': q.choices,
+            'answer': q.answer
+        })
+    with open(outfile, 'w') as f:
+        json.dump(data, f)
 
 ############################################################################################
 # Main Function
 ############################################################################################
 
 if __name__ == "__main__":
+    # Set timestamp
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
     # Setup survey
+    get_questions()
     # Launch survey
     main_menu()
 
